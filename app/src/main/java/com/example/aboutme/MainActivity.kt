e67683597a497645b53e4e58c782b251e369ff68
l3l3vill <lg.villamil.guerrero@gmail.com>
2020-03-19 22:39:09 +0100

    Presentation - data binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+    private val myName : MyName = MyName("Geraldine Vilamil")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.myName = myName
+        binding.button.setOnClickListener {
+            addNickName(it)
+        }
+
+    }
+
+    fun addNickName(view : View){
+
+
+        binding.apply {
+            //nickName.text = binding.editText.text
+            myName?.nickName = editText.text.toString()
+            invalidateAll()
+            editText.visibility = View.GONE
+            view.visibility = View.GONE
+            nickName.visibility = View.VISIBLE
+        }
+
+
+
+        //hide keyboard
+        val imn = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imn.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
